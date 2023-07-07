82e30ca7132a5f1bc0cb8d15fa34550550465453
andrefelype <andrefilype21122014@gmail.com>
2023-07-07 16:42:17 -0300

    add novas cores

--- js/script.js
+++ js/script.js
@@ -5,26 +5,57 @@ const esfera3 = document.querySelector('.meta3');
 const esfera4 = document.querySelector('.meta4');
 const tela = document.querySelector('body');
 
-let n = 1
+var n = 0
 
 tela.addEventListener('click', () => {
 
-    if(n === 1) {
+    
 
-        esfera1.classList.replace('meta1','metaall')
-        esfera2.classList.replace('meta2','metaall')
-        esfera3.classList.replace('meta3','metaall')
-        esfera4.classList.replace('meta4','metaall')
+    if(n === 0) {
+
+        esfera1.classList.replace('meta1','meta-red')
+        esfera2.classList.replace('meta2','meta-red')
+        esfera3.classList.replace('meta3','meta-red')
+        esfera4.classList.replace('meta4','meta-red')
+
+        n++
+
+    } else if(n === 1) {
+
+        esfera1.classList.replace('meta-red','meta-purple')
+        esfera2.classList.replace('meta-red','meta-purple')
+        esfera3.classList.replace('meta-red','meta-purple')
+        esfera4.classList.replace('meta-red','meta-purple')
+
+        n++
+
+    } else if(n === 2) {
+
+        esfera1.classList.replace('meta-purple','meta-green')
+        esfera2.classList.replace('meta-purple','meta-green')
+        esfera3.classList.replace('meta-purple','meta-green')
+        esfera4.classList.replace('meta-purple','meta-green')
+
+        n++
+
+    } else if(n === 3) {
+
+        esfera1.classList.replace('meta-green','meta-yellow')
+        esfera2.classList.replace('meta-green','meta-yellow')
+        esfera3.classList.replace('meta-green','meta-yellow')
+        esfera4.classList.replace('meta-green','meta-yellow')
+
+        n = 4
+
+    } else if(n === 4) {
+
+        esfera1.classList.replace('meta-yellow','meta1')
+        esfera2.classList.replace('meta-yellow','meta2')
+        esfera3.classList.replace('meta-yellow','meta3')
+        esfera4.classList.replace('meta-yellow','meta4')
 
         n = 0
 
-    } else {
-
-        esfera1.classList.replace('metaall','meta1')
-        esfera2.classList.replace('metaall','meta2')
-        esfera3.classList.replace('metaall','meta3')
-        esfera4.classList.replace('metaall','meta4')
-
-        n = 1
     }
+    
 })
